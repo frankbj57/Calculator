4adee876f6cbd88a8b6aee0f260dd6200a8bb661
frankbj57 <frabj@ase.au.dk>
2018-01-30 22:38:35 +0100

    Corrected add

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -7,7 +7,7 @@ namespace Calculator
         public double Add(double a, double b)
         {
 // Add comment
-            return a + b + 5;
+            return a + b;
         }
 
         public double Subtract(double a, double b)
