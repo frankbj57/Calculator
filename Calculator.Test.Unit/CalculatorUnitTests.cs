8bda0c1f6057bf33b08fb9686b6f5d3a9d145c30
Frank B. Jakobsen <frabj@ase.au.dk>
2019-02-01 14:48:57 +0100

    Reorganized tests and added comments

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -11,19 +11,33 @@ namespace Calculator.Test.Unit
     {
         private Calculator _uut;
 
+        // Using the SetUp feature, saves coding (part of) the Arrange step
         [SetUp]
         public void Setup()
         {
             _uut = new Calculator();
         }
 
+        // A single test case, with fixed input and expected output
+        [Test]
+        public void Add_Add2And3_ResultIs5()
+        {
+            // Act
+            var result = _uut.Add(2, 3);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        // The TestCase feature allows reusing test code for diffent input and expected output
         [TestCase(3, 2, 5)]
         [TestCase(-3, -2, -5)]
         [TestCase(-3, 2, -1)]
         [TestCase(3, -2, 1)]
-        [TestCase(3, -3, 0)]
+        [TestCase(3, 0, 3)]
         public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
         {
+            // Combining the Act and Assert steps is also possible
             Assert.That(_uut.Add(a, b), Is.EqualTo(result));
         }
 
@@ -31,7 +45,6 @@ namespace Calculator.Test.Unit
         [TestCase(3, 2, 1)]
         [TestCase(-3, -2, -1)]
         [TestCase(-3, 2, -5)]
-        [TestCase(3, -2, 5)]
         public void Subtract_SubtractPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
         {
             Assert.That(_uut.Subtract(a, b), Is.EqualTo(result));
@@ -50,17 +63,6 @@ namespace Calculator.Test.Unit
             Assert.That(_uut.Multiply(a, b), Is.EqualTo(result));
         }
 
-        [Test]
-        public void Add_AddZero_ResultIsTheSame()
-        {
-            Assert.That(_uut.Add(4, 0), Is.EqualTo(4));
-        }
-
-        [Test]
-        public void Subtract_SubtractZero_ResultIsTheSame()
-        {
-            Assert.That(_uut.Subtract(4, 0), Is.EqualTo(4));
-        }
 
         [TestCase(2, 3, 8)]
         [TestCase(2, -3, 0.125)]
@@ -69,10 +71,22 @@ namespace Calculator.Test.Unit
         [TestCase(1, -10, 1)]
         [TestCase(10, 0, 1)]
         [TestCase(4, 0.5, 2.0)]
-		[TestCase(9, 0.5, 3.0)]
+        [TestCase(9, 0.5, 3.0)]
         public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
         {
             Assert.That(_uut.Power(x, exp), Is.EqualTo(result));
         }
+
+        // For floating point results, exact equal comparison can be tricky
+        // Use EqualTo modifier Within
+        [Test]
+        public void Power_SquareRootHalf_ResultIsWithinPrecision()
+        {
+            // Raising a number to 1/2 is the same as taking the square root
+            double result = _uut.Power(0.5, 0.5);
+
+            Assert.That(result, Is.EqualTo(0.707107).Within(0.0000005));
+        }
+
     }
 }
